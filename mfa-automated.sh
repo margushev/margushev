7b9fd5d11fcef9d1c92a5057ecf671564f316ccc
Atanas Margushev <atanas.margushev@gmail.com>
2023-11-23 11:31:44 +0200

    added mfa-automated.sh

---
+++ mfa-automated.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PS3='Choose AWS Account: '
+account=("Eduspire" "Taylor&Hart" "VWFS" "EN" "Quit")
+select fav in "${account[@]}"; do
+    case $fav in
+	 "EN")
+            read -p "MFA Code: " mfa
+            sh ~/mfa.sh $mfa en en-mfa
+            break
+            ;;
+        "Eduspire")
+	    read -p "MFA Code: " mfa      
+            sh ~/mfa.sh $mfa eduspire eduspire-mfa
+	    break
+            ;;
+        "Taylor&Hart")
+            read -p "MFA Code: " mfa
+            sh ~/mfa.sh $mfa taylorandhart taylorandhart-mfa
+	    break
+            ;;
+	"VWFS")
+	    AWS_PROFILE=vwfs
+	    KUBE_CONFIG_PATH="~/.kube/config"
+	    saml2aws login --force
+            break
+            ;;
+	"Quit")
+	    echo "User requested exit"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
